4726443b536fb19143fdc803fbcdcf729a6095d7
Sati <rock_sal@mail.ru>
2022-09-27 05:27:49 +0600

    Calendar

---
+++ script.js
@@ -0,0 +1,14 @@
+"use script";
+
+const date = new Date();
+
+const renderCalendar = () => {
+
+    date.setDate(1);
+
+    const monthDays = document.querySelector(".days");
+
+    
+};
+
+renderCalendar();
